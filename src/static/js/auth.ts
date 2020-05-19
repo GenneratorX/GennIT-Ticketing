GenneratorX/GennIT-Ticketing

commit 54270d05a3bad305a38d5b3fb3129271de28b570

Clean and split all CSS files and use HTTP GET for username/email checks
*Split all CSS into multiple files to improve readability
*Remove redundant CSS classes and properties
*Merge CSS classes where possible
*Use GET instead of POST for username and email availability checks
because they dont change the application state

src/static/css/auth.css
@@ -1,0 +1,88 @@
+h1 {
+  font-size: 2rem;
+  font-weight: 200;
+  margin-top: 0;
+}
+
+#username {
+  background: url(https://static.gennerator.com/img/svg/username.svg) no-repeat 0.7rem 50%/1.4rem 1.4rem;
+}
+
+#password,
+#repeatPassword {
+  background: url(https://static.gennerator.com/img/svg/password.svg) no-repeat 0.7rem 50%/1.4rem 1.4rem;
+}
+
+#email {
+  background: url(https://static.gennerator.com/img/svg/email.svg) no-repeat 0.7rem 50%/1.4rem 1.4rem;
+}
+
+.info {
+  font-size: 1rem;
+  font-weight: 700;
+  margin-top: 1rem;
+  max-width: 20rem;
+}
+
+.info-content {
+  display: block;
+  font-weight: 200;
+  margin-top: 0.4rem;
+}
+
+div.auth {
+  font-size: 1rem;
+  margin-top: 1.5rem;
+  user-select: none;
+}
+
+a {
+  color: #fff;
+  cursor: pointer;
+  display: block;
+  font-size: 1.1rem;
+  font-weight: 700;
+  margin-top: 0.5rem;
+}
+
+a:hover {
+  text-decoration: underline;
+}
+
+input.login {
+  min-width: 18rem;
+}
+
+.submitB {
+  background: #363636;
+  border: 0;
+  border-radius: 50%;
+  color: #fff;
+  font-size: 2rem;
+  font-weight: 700;
+  height: 2em;
+  margin-top: 2rem;
+  padding: 1rem;
+  width: 2em;
+}
+
+.submitB:hover {
+  background: #09776c;
+}
+
+.submitB:focus {
+  background: #09776c;
+  outline: 0;
+}
+
+.checkbox-label {
+  cursor: pointer;
+  font-size: 1.2rem;
+  margin-top: 1rem;
+  user-select: none;
+}
+
+.checkbox-label:focus {
+  outline: 0;
+  text-shadow: 5px 5px 10px #09776c;
+}

src/static/css/style.css
@@ -12,6 +12,33 @@ body {
   height: 100vh;
 }
 
+input,
+select {
+  background: #232323;
+  border: 2px #363636 solid;
+  border-radius: 15px;
+  color: #fff;
+  font-family: 'Raleway', Arial, sans-serif;
+  font-size: 1rem;
+  font-weight: 700;
+  margin-top: 1rem;
+  min-height: 3rem;
+  padding: 0.1rem 0.5rem 0.1rem 2.7rem;
+}
+
+input:focus {
+  box-shadow: 0 0 5px #363636;
+  outline: 0;
+}
+
+input.green {
+  border-color: #09776c;
+}
+
+input.red {
+  border-color: #d3312e;
+}
+
 .content {
   align-items: safe center;
   color: #fff;
@@ -23,131 +50,6 @@ body {
   min-height: min-content;
   min-width: min-content;
   text-align: center;
-}
-
-/* ===================== LOGIN ===================== */
-#auth h1 {
-  font-size: 2rem;
-  font-weight: 200;
-  margin-top: 0;
-}
-
-#auth #username {
-  background: url(https://static.gennerator.com/img/svg/username.svg) no-repeat 0.7rem 50%/1.4rem 1.4rem;
-}
-
-#auth #password {
-  background: url(https://static.gennerator.com/img/svg/password.svg) no-repeat 0.7rem 50%/1.4rem 1.4rem;
-}
-
-#auth #repeatPassword {
-  background: url(https://static.gennerator.com/img/svg/password.svg) no-repeat 0.7rem 50%/1.4rem 1.4rem;
-}
-
-#auth #email {
-  background: url(https://static.gennerator.com/img/svg/email.svg) no-repeat 0.7rem 50%/1.4rem 1.4rem;
-}
-
-#auth input.green {
-  border-color: #09776c;
-}
-
-#auth input.red {
-  border-color: #d3312e;
-}
-
-#auth .info {
-  font-size: 1rem;
-  font-weight: 700;
-  margin-top: 1rem;
-  max-width: 20rem;
-}
-
-#auth .info-content {
-  display: block;
-  font-weight: 200;
-  margin-top: 0.4rem;
-}
-
-div.auth {
-  display: inline-block;
-  font-size: 1rem;
-  margin-top: 1.5rem;
-  user-select: none;
-}
-
-a.auth {
-  color: #fff;
-  cursor: pointer;
-  display: block;
-  font-size: 1.1rem;
-  font-weight: 700;
-  margin-top: 0.5rem;
-}
-
-a.auth:hover,
-a.right:hover {
-  text-decoration: underline;
-}
-
-a.login {
-  font-size: 1.3rem;
-  margin: auto 0.5rem auto auto;
-  text-decoration: none;
-}
-
-input.login {
-  background: transparent;
-  border: 2px #363636 solid;
-  border-radius: 15px;
-  color: #fff;
-  display: block;
-  font-family: 'Raleway', Arial, sans-serif;
-  font-size: 1rem;
-  font-weight: 700;
-  margin-top: 1rem;
-  min-height: 3rem;
-  min-width: 18rem;
-  padding: 0.1rem 0.5rem 0.1rem 2.7rem;
-}
-
-input.login:focus {
-  box-shadow: 0 0 5px #363636;
-  outline: 0;
-}
-
-input.submitB {
-  background: #363636;
-  border: 0;
-  border-radius: 50%;
-  color: #fff;
-  font-size: 2rem;
-  font-weight: 700;
-  height: 2em;
-  margin-top: 2rem;
-  padding: 1rem;
-  width: 2em;
-}
-
-input.submitB:hover {
-  background: #09776c;
-}
-
-input.submitB:focus {
-  background: #09776c;
-  outline: 0;
-}
-
-.checkbox-label {
-  cursor: pointer;
-  font-size: 1.2rem;
-  margin-top: 1rem;
-  user-select: none;
-}
-
-.checkbox-label:focus {
-  outline: 0;
-  text-shadow: 5px 5px 10px #09776c;
 }
 
 /* =========================== NAVIGATION BAR =========================== */
@@ -290,199 +192,6 @@ div.main {
   }
 }
 
-/* ============================= USER INFO ============================= */
-.user-page {
-  align-items: center;
-  display: grid;
-  grid-column-gap: 0.5rem;
-  grid-row-gap: 0;
-  grid-template-columns: repeat(4, 1fr);
-  grid-template-rows: repeat(3, 1fr);
-  justify-items: center;
-  overflow: auto;
-}
-
-.profile {
-  align-self: end;
-  grid-area: 1 / 1 / 2 / 5;
-}
-
-.user-info {
-  align-self: start;
-  grid-area: 2 / 1 / 4 / 3;
-}
-
-.user-performance {
-  align-self: start;
-  grid-area: 2 / 3 / 4 / 4;
-}
-
-.user-friends {
-  align-self: start;
-  grid-area: 2 / 4 / 4 / 5;
-}
-
-.profile-picture {
-  background: #ed9c27;
-  border-radius: 50%;
-  font-size: 5rem;
-  font-weight: 700;
-  height: 10rem;
-  line-height: 10rem;
-  margin: 0 auto;
-  text-align: center;
-  width: 10rem;
-}
-
-.profile-name {
-  font-size: 2rem;
-  font-weight: 700;
-  margin-top: 1rem;
-}
-
-.info-box h2 {
-  margin-bottom: 1rem;
-}
-
-.info-box div {
-  margin-top: 0.3rem;
-}
-
-.info-box {
-  background: #363636;
-  font-size: 1.2rem;
-  margin-top: 3rem;
-  min-width: 50%;
-  padding: 2rem;
-  text-align: left;
-  width: 100%;
-}
-
-.box-title {
-  display: flex;
-  margin-bottom: 1rem;
-}
-
-.box-title-description {
-  align-self: center;
-  display: flex;
-  font-size: 1.7rem;
-  font-weight: 700;
-}
-
-.box-title-edit {
-  align-self: center;
-  border-radius: 5px;
-  display: flex;
-  margin-left: auto;
-  padding: 0.5rem;
-  width: 2.4rem;
-}
-
-.box-title-edit:hover {
-  background: #09776c;
-  cursor: pointer;
-}
-
-.edit-user-info {
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-}
-
-.edit-user-info input,
-select {
-  background: #232323;
-  border: 2px #363636 solid;
-  border-radius: 15px;
-  color: #fff;
-  font-family: 'Raleway', Arial, sans-serif;
-  font-size: 1rem;
-  font-weight: 700;
-  margin-top: 1rem;
-  min-height: 3rem;
-  padding: 0.1rem 0.5rem 0.1rem 2.7rem;
-}
-
-.edit-user-info input:focus,
-select:focus {
-  box-shadow: 0 0 5px #363636;
-  outline: 0;
-}
-
-#firstName {
-  flex: 1;
-  min-width: 18rem;
-}
-
-#lastName {
-  flex: 1;
-  min-width: 18rem;
-}
-
-#email {
-  flex: 1;
-  min-width: 100%;
-}
-
-#birthDateDay {
-  flex: 1;
-  min-width: 5rem;
-}
-
-#birthDateMonth {
-  flex: 1;
-  min-width: 18rem;
-}
-
-#birthDateYear {
-  flex: 1;
-  min-width: 6rem;
-}
-
-#gender {
-  flex: 1;
-  min-width: 15rem;
-}
-
-#phoneNumber {
-  flex: 1;
-  min-width: 18rem;
-}
-
-#submitButton {
-  flex: 1;
-  min-width: 100%;
-  padding: 0.7rem;
-}
-
-#submitButton:hover {
-  background: #09776c;
-}
-
-@media screen and (max-width: 1024px) {
-  .user-page {
-    display: flex;
-    flex-direction: column;
-  }
-
-  .profile {
-    margin: auto auto 3rem;
-  }
-
-  .user-info {
-    margin: 0 auto auto;
-  }
-
-  .user-performance {
-    margin: 1rem auto auto;
-  }
-
-  .user-friends {
-    margin: 1rem auto auto;
-  }
-}
-
 /* ============================== SNACKBAR ============================== */
 #snackbar {
   align-items: center;
@@ -548,10 +257,6 @@ select:focus {
     bottom: 0;
     opacity: 0;
   }
-}
-
-.inline {
-  display: inline-block;
 }
 
 .bold {

src/static/js/auth.ts
@@ -335,7 +335,7 @@ function usernameInputKeyUp() {
         break;
       case 'signUp':
         delay = setTimeout(() => {
-          request('POST', '/usernameExists', { 'username': usernameInput.value })
+          request('GET', `/usernameExists?username=${usernameInput.value}`)
             .then(response => {
               if (response['exists'] === false) {
                 setBorderColor(usernameInput, 'green');
@@ -407,7 +407,7 @@ function emailInputKeyUp() {
     switch (currentWindow) {
       case 'signUp':
         delay = setTimeout(() => {
-          request('POST', '/emailExists', { 'email': emailInput.value })
+          request('GET', `/emailExists?email=${emailInput.value}`)
             .then(response => {
               if (response['exists'] === false) {
                 setBorderColor(emailInput, 'green');

src/routes/auth.ts
@@ -62,9 +62,9 @@ router.route('/signin')
   }).all(util.httpErrorAllowOnlyPost);
 
 router.route('/usernameExists')
-  .post(util.checkJsonHeader, function(req, res, next) {
-    if (typeof req.body.username === 'string') {
-      auth.usernameExists(req.body.username)
+  .get(function(req, res, next) {
+    if (typeof req.query.username === 'string') {
+      auth.usernameExists(req.query.username)
         .then(exists => {
           res.json({ exists: exists });
         })
@@ -72,12 +72,12 @@ router.route('/usernameExists')
     } else {
       res.status(422).json({ error: 'invalid username' });
     }
-  }).all(util.httpErrorAllowOnlyPost);
+  }).all(util.httpErrorAllowOnlyGet);
 
 router.route('/emailExists')
-  .post(util.checkJsonHeader, function(req, res, next) {
-    if (typeof req.body.email === 'string') {
-      auth.emailExists(req.body.email)
+  .get(function(req, res, next) {
+    if (typeof req.query.email === 'string') {
+      auth.emailExists(req.query.email)
         .then(exists => {
           res.json({ exists: exists });
         })
@@ -85,7 +85,7 @@ router.route('/emailExists')
     } else {
       res.status(422).json({ error: 'invalid email' });
     }
-  }).all(util.httpErrorAllowOnlyPost);
+  }).all(util.httpErrorAllowOnlyGet);
 
 router.route('/forgotPassword')
   .post(util.checkJsonHeader, function(req, res, next) {

src/static/css/user.css
@@ -1,0 +1,164 @@
+.user-page {
+  align-items: center;
+  display: grid;
+  grid-column-gap: 0.5rem;
+  grid-row-gap: 0;
+  grid-template-columns: repeat(4, 1fr);
+  grid-template-rows: repeat(3, 1fr);
+  justify-items: center;
+  overflow: auto;
+}
+
+.profile {
+  align-self: end;
+  grid-area: 1 / 1 / 2 / 5;
+}
+
+.user-info {
+  align-self: start;
+  grid-area: 2 / 1 / 4 / 3;
+}
+
+.user-performance {
+  align-self: start;
+  grid-area: 2 / 3 / 4 / 4;
+}
+
+.user-friends {
+  align-self: start;
+  grid-area: 2 / 4 / 4 / 5;
+}
+
+.profile-picture {
+  background: #ed9c27;
+  border-radius: 50%;
+  font-size: 5rem;
+  font-weight: 700;
+  height: 10rem;
+  line-height: 10rem;
+  margin: 0 auto;
+  text-align: center;
+  width: 10rem;
+}
+
+.profile-name {
+  font-size: 2rem;
+  font-weight: 700;
+  margin-top: 1rem;
+}
+
+.info-box h2 {
+  margin-bottom: 1rem;
+}
+
+.info-box div {
+  margin-top: 0.3rem;
+}
+
+.info-box {
+  background: #363636;
+  font-size: 1.2rem;
+  margin-top: 3rem;
+  min-width: 50%;
+  padding: 2rem;
+  text-align: left;
+  width: 100%;
+}
+
+.box-title {
+  display: flex;
+  margin-bottom: 1rem;
+}
+
+.box-title-description {
+  align-self: center;
+  display: flex;
+  font-size: 1.7rem;
+  font-weight: 700;
+}
+
+.box-title-edit {
+  align-self: center;
+  border-radius: 5px;
+  display: flex;
+  margin-left: auto;
+  padding: 0.5rem;
+  width: 2.4rem;
+}
+
+.box-title-edit:hover {
+  background: #09776c;
+  cursor: pointer;
+}
+
+.edit-user-info {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+
+#firstName,
+#lastName,
+#birthDateMonth,
+#phoneNumber {
+  flex: 1;
+  min-width: 18rem;
+}
+
+#email {
+  flex: 1;
+  min-width: 100%;
+}
+
+#birthDateDay {
+  flex: 1;
+  min-width: 5rem;
+}
+
+#birthDateYear {
+  flex: 1;
+  min-width: 6rem;
+}
+
+#gender {
+  flex: 1;
+  min-width: 15rem;
+}
+
+#submitButton {
+  flex: 1;
+  min-width: 100%;
+  padding: 0.7rem;
+}
+
+#submitButton:hover {
+  background: #09776c;
+}
+
+#submitButton:focus {
+  background: #09776c;
+  outline: 0;
+}
+
+@media screen and (max-width: 1024px) {
+  .user-page {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .profile {
+    margin: auto auto 3rem;
+  }
+
+  .user-info {
+    margin: 0 auto auto;
+  }
+
+  .user-performance {
+    margin: 1rem auto auto;
+  }
+
+  .user-friends {
+    margin: 1rem auto auto;
+  }
+}

src/views/auth.ejs
@@ -8,6 +8,7 @@
     <meta property="og:url" content="https://master.gennerator.com/auth">
     <meta property="og:image" content="https://static.gennerator.com/android-512.png">
     <meta property="og:description" content="Autentificare utilizator, creare cont utilizator și recuperare credențiale.">
+    <link rel="preload" href="https://static.gennerator.com/css/auth.css" as="style">
     <script nonce="<%- cspNonce %>" src="https://static.gennerator.com/js/auth.js" defer></script>
   </head>
   <body>

src/views/user.ejs
@@ -3,6 +3,7 @@
   <head>
     <%- include('partials/header') %>
     <title><%- username %> :: GennIT Ticketing</title>
+    <link rel="preload" href="https://static.gennerator.com/css/user.css" as="style">
     <script nonce="<%- cspNonce %>" src="https://static.gennerator.com/js/index.js" defer></script>
     <script nonce="<%- cspNonce %>" src="https://static.gennerator.com/js/user.js" defer></script>
   </head>

commit d3f26069bd96dfd2d3377bea163cad01b90b7a19

Fix signup checkbox positioning and make it fancy
*Fix checkbox positioning broken by 54270d05a3bad305a38d5b3fb3129271de28b570
*Make the checkbox consistent across browsers
*Update dependencies

src/static/css/auth.css
@@ -75,14 +75,38 @@ input.login {
   outline: 0;
 }
 
-.checkbox-label {
-  cursor: pointer;
+.cbox {
   font-size: 1.2rem;
-  margin-top: 1rem;
+  line-height: 1.5rem;
+  margin-top: 1.5rem;
+}
+
+.cbox label::before {
+  background: #363636;
+  border-radius: 7px;
+  content: "\00a0";
+  display: inline-block;
+  margin-right: 0.5rem;
+  text-align: center;
+  width: 1.5rem;
+}
+
+.cbox label:hover::before {
+  background: #303030;
+}
+
+.cbox label {
+  cursor: pointer;
   user-select: none;
 }
 
-.checkbox-label:focus {
-  outline: 0;
-  text-shadow: 5px 5px 10px #09776c;
+.cbox input:checked ~ label::before {
+  content: "\2714";
+}
+
+.cbox input {
+  margin-right: -1.2rem;
+  margin-top: 0;
+  min-height: 1rem;
+  opacity: 0;
 }

src/static/css/style.css
@@ -26,9 +26,15 @@ select {
   padding: 0.1rem 0.5rem 0.1rem 2.7rem;
 }
 
-input:focus {
+input:focus,
+select:focus {
   box-shadow: 0 0 5px #363636;
   outline: 0;
+}
+
+input:disabled,
+select:disabled {
+  background: #454545;
 }
 
 input.green {

src/static/js/auth.ts
@@ -40,7 +40,7 @@ const authElements = {
   /**
    * Agreement button
    */
-  checkBoxLabel: document.getElementById('chkLabel') as HTMLLabelElement,
+  checkBoxDiv: document.getElementById('cbox') as HTMLDivElement,
   /**
    * Information box
    */
@@ -64,7 +64,7 @@ function displaySignIn() {
     authTitle,
     authMenuItem1,
     authMenuItem2,
-    checkBoxLabel,
+    checkBoxDiv,
     emailInput,
     infoBox,
     passwordInput,
@@ -78,7 +78,7 @@ function displaySignIn() {
     case 'signUp':
       submitForm.removeChild(repeatPasswordInput);
       submitForm.removeChild(emailInput);
-      submitForm.removeChild(checkBoxLabel);
+      submitForm.removeChild(checkBoxDiv);
       passwordInput.setAttribute('autocomplete', 'current-password');
       break;
     case 'forgotPassword':
@@ -261,26 +261,32 @@ function addRepeatPasswordInput() {
  * Adds an accept agreement checkbox
  */
 function addAgreementCheckbox() {
-  if (authElements.checkBoxLabel === null) {
-    const checkBoxLabel = document.createElement('label');
-    setAttributes(checkBoxLabel, {
-      'class': 'checkbox-label',
-      'id': 'chkLabel',
+  if (authElements.checkBoxDiv === null) {
+    const checkBoxDiv = document.createElement('div');
+    setAttributes(checkBoxDiv, {
+      'class': 'cbox',
+      'id': 'cbox',
     });
-    checkBoxLabel.textContent = 'Accept termenii și condițiile';
 
     const checkBoxInput = document.createElement('input');
     setAttributes(checkBoxInput, {
-      'id': 'chkBox',
-      'required': '',
       'type': 'checkbox',
+      'required': '',
+      'id': 'box',
     });
-    checkBoxLabel.insertAdjacentElement('afterbegin', checkBoxInput);
 
-    authElements.checkBoxLabel = checkBoxLabel;
-    authElements.submitForm.insertBefore(checkBoxLabel, authElements.submitButton);
+    const checkBoxLabel = document.createElement('label');
+    setAttributes(checkBoxLabel, {
+      'for': 'box',
+    });
+    checkBoxLabel.textContent = 'Accept termenii și condițiile';
+
+    checkBoxDiv.appendChild(checkBoxInput);
+    checkBoxDiv.appendChild(checkBoxLabel);
+    authElements.checkBoxDiv = checkBoxDiv;
+    authElements.submitForm.insertBefore(checkBoxDiv, authElements.submitButton);
   } else {
-    authElements.submitForm.insertBefore(authElements.checkBoxLabel, authElements.submitButton);
+    authElements.submitForm.insertBefore(authElements.checkBoxDiv, authElements.submitButton);
   }
 }
 